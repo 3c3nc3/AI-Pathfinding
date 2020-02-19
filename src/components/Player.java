ef5c525665c7971c16044efa17511e100c8b7f43
Emma <bipbop523@gmail.com>
2020-02-19 18:31:51 -0500

    Projectiles functional, no hitboxes

--- src/components/Player.java
+++ src/components/Player.java
@@ -4,6 +4,7 @@ import java.awt.Graphics;
 
 import app.Keybinding;
 import app.Mousebinding;
+import app.util.Direction;
 import app.util.Point;
 import fx.assetloaders.Assets;
 import main.Updateable;
@@ -16,13 +17,23 @@ public class Player implements Updateable {
 
     private static Point point = new Point(((540 / 2) - 32), ((480 / 2) - 32));;
     private static ControlMode controlMode = ControlMode.Keyboard;
-
+    private Projectile projectile = new Projectile(assets.getTexture("bad-dot"), 5, 10);
 
     public int playerSpeed = 3;
 
+    public Player() {
+        projectile.setTermFactors(new Projectile.TermFactors(){
+            public boolean factor(Projectile.ProjectileTemplate pt) {
+                if (pt.x > 580 || pt.x < 0 || pt.y < 0 || pt.y > 480) {
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
     @Override
     public void update() {
-
         switch(controlMode) {
             case Keyboard:
                 keyboardControl();
@@ -46,25 +57,40 @@ public class Player implements Updateable {
         } else if (point.y > 409) {
             point.y = 409;
         }
+
+        projectile.update();
     }
 
     @Override
     public void render(Graphics g) {
+        projectile.render(g);
         g.drawImage(assets.getTexture("player"), point.x, point.y, null);
     }
 
     private void keyboardControl() {
-        if (keybinding.getKey("up")) {
+        if (keybinding.getKey("w")) {
             point.y = point.y - playerSpeed;
         }
-        if (keybinding.getKey("down")) {
+        if (keybinding.getKey("s")) {
             point.y = point.y + playerSpeed;
         }
-        if (keybinding.getKey("left")) {
+        if (keybinding.getKey("a")) {
             point.x = point.x - playerSpeed;
         }
-        if (keybinding.getKey("right")) {
+        if (keybinding.getKey("d")) {
             point.x = point.x + playerSpeed;
+        }
+        if (keybinding.getKey("up")) {
+            projectile.createProjectile(point.x, point.y, Direction.up);
+        }
+        if (keybinding.getKey("down")) {
+            projectile.createProjectile(point.x, point.y, Direction.down);
+        }
+        if (keybinding.getKey("left")) {
+            projectile.createProjectile(point.x, point.y, Direction.left);
+        }
+        if (keybinding.getKey("right")) {
+            projectile.createProjectile(point.x, point.y, Direction.right);
         }
     }
 
--- src/components/Projectile.java
+++ src/components/Projectile.java
@@ -1,5 +1,100 @@
 package components;
 
-public class Projectile {
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.util.ArrayList;
 
+import app.util.Direction;
+import main.Updateable;
+
+public class Projectile implements Updateable {
+    private BufferedImage texture;
+    private ArrayList<ProjectileTemplate> existingProjectiles = new ArrayList<>();
+
+    private TermFactors termFactors;
+    private long fireRate, lastFired = 0;
+    private int speed = 1;
+    
+    public Projectile(BufferedImage texture) {
+        this.texture = texture;
+    }
+
+    public Projectile(BufferedImage texture, int speed, int fireRate) {
+        this.texture = texture;
+        this.fireRate = fireRate;
+        this.speed = speed;
+    }
+    
+    public void createProjectile(int x, int y, Direction d) {
+        if (lastFired > fireRate) {
+            existingProjectiles.add(new ProjectileTemplate(x, y, d));
+            lastFired = 0;
+        }
+    }
+
+    public void setFirerate(int rate) {
+        fireRate = rate;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setTermFactors(TermFactors tf) {
+        termFactors = tf;
+    }
+
+    public void update() {
+        System.out.println(existingProjectiles.size());
+        ArrayList<ProjectileTemplate> termList = new ArrayList<>();
+        for(ProjectileTemplate pt : existingProjectiles) {
+            switch(pt.direction) {
+                case up:
+                    pt.y = pt.y - speed;
+                    break;
+                case right:
+                    pt.x = pt.x + speed;
+                    break;
+                case down:
+                    pt.y = pt.y + speed;
+                    break;
+                case left:
+                    pt.x = pt.x - speed;
+                    break;
+                default:
+                    System.err.println("Invalid projectile direction! How did you manage that?");
+                    break;
+            }
+            if (termFactors.factor(pt)) {
+                termList.add(pt);
+            }
+        }
+        for(ProjectileTemplate pt : termList) {
+            existingProjectiles.remove(pt);
+        }
+        if (lastFired < 370000) {
+            lastFired++;
+        }
+    }
+    
+    public void render(Graphics g) {
+        for(ProjectileTemplate pt : existingProjectiles) {
+            g.drawImage(texture, pt.x, pt.y, null);
+        }
+    }
+    
+    public static class ProjectileTemplate {
+        public Direction direction;
+        public int x, y;
+        
+        public ProjectileTemplate(int startingX, int startingY, Direction d) {
+            direction = d;
+            x = startingX;
+            y = startingY;
+        }
+    }
+
+    public static interface TermFactors {
+        public boolean factor(ProjectileTemplate pt);
+    }
 }
